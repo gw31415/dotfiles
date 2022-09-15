e87b1e4b9faa73135850a58c17e751a5d5856cca
gw31415 <git@amas.dev>
2022-09-15 14:52:02 +0900

    formmat wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm';
 return {
 	color_scheme = "Tomorrow Night",
-	default_prog = {"/usr/local/bin/fish", "-l"},
+	default_prog = { "/usr/local/bin/fish", "-l" },
 	hide_tab_bar_if_only_one_tab = true,
 	font = wezterm.font("HackGenNerd Console"),
 	font_size = 13,
