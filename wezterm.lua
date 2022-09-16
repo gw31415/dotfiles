98ea0b68256fc51a758c216c05eaecb6609d4ecf
gw31415 <git@amas.dev>
2022-09-16 22:54:18 +0900

    change fontsize & initial windowsize wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ return {
 	default_prog = { "/usr/local/bin/fish", "-l" },
 	hide_tab_bar_if_only_one_tab = true,
 	font = wezterm.font("HackGenNerd Console"),
-	font_size = 13,
-	initial_cols = 140,
-	initial_rows = 45,
+	font_size = 14,
+	initial_cols = 130,
+	initial_rows = 40,
 }
