061cae79f3556402f383573edd39df893ec2292b
gw31415 <git@amas.dev>
2022-12-04 17:28:09 +0900

    add treesitter-unit

--- init.lua
+++ init.lua
@@ -564,6 +564,18 @@ require 'jetpack.packer'.startup(function(use)
 					additional_vim_regex_highlighting = false,
 				},
 			}
+			vim.fn['jetpack#load']('treesitter-unit')
+		end
+	}
+	use {
+		'David-Kunz/treesitter-unit',
+		opt = true,
+		config = function()
+			local opts = { noremap = true, silent =true }
+			vim.api.nvim_set_keymap('x', 'iu', ':lua require"treesitter-unit".select()<CR>', opts)
+			vim.api.nvim_set_keymap('x', 'au', ':lua require"treesitter-unit".select(true)<CR>', opts)
+			vim.api.nvim_set_keymap('o', 'iu', ':<c-u>lua require"treesitter-unit".select()<CR>', opts)
+			vim.api.nvim_set_keymap('o', 'au', ':<c-u>lua require"treesitter-unit".select(true)<CR>', opts)
 		end
 	}
 	use {
