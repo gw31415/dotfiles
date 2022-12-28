6273a629eff124880c84b98ae439da5f71386b7c
gw31415 <git@amas.dev>
2022-12-28 12:52:57 +0900

    changed initial size

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,6 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	font = wezterm.font("HackGenNerd Console"),
 	font_size = 14,
-	initial_cols = 130,
-	initial_rows = 40,
+	initial_cols = 150,
+	initial_rows = 50,
 }
