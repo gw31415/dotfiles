b74639b020124f21b9d7f9137135cfebee360a78
gw31415 <git@amas.dev>
2023-06-09 20:38:18 +0900

    config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,6 +6,6 @@ end
 export WASMER_DIR="$HOME/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
 
-if type -q docker
-	export DOCKER_HOST=$(docker context inspect --format '{{.Endpoints.docker.Host}}')
-end
+# if type -q docker
+# export DOCKER_HOST=$(docker context inspect --format '{{.Endpoints.docker.Host}}')
+# end
