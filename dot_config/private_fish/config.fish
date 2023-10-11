f0ab018fbfd45a2ab3c5c13b82e6175cc93eec67
gw31415 <git@amas.dev>
2023-10-11 15:47:11 +0900

    update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -14,18 +14,6 @@ if type -q rye
 	set -x PATH "$HOME/.rye/shims" $PATH
 end
 
-# Wasmer
-export WASMER_DIR="$HOME/.wasmer"
-[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
-
-# if type -q docker
-# export DOCKER_HOST=$(docker context inspect --format '{{.Endpoints.docker.Host}}')
-# end
-
-if type -q goneovim
-	abbr -a nv goneovim --nofork
-end
-
 # Rust compile cache
 if type -q sccache
 	export RUSTC_WRAPPER=(which sccache)
