4edf07a6aecc90115a1364860f3885db0a2b4e6b
gw31415 <git@amas.dev>
2023-11-13 19:22:05 +0900

    update

--- dot_config/private_fish/functions/docker_lima.fish
+++
@@ -1,9 +0,0 @@
-function docker_lima
-	# To use Docker with lima you need to set up a docker template:
-	# $ limactl start --name=docker template://docker
-	limactl start docker
-
-	# In Docker, you need to set up a connection to lima:
-	# $ docker context create lima-docker --docker "host=unix:///Users/ama/.lima/docker/sock/docker.sock"
-	docker context use lima-docker
-end
