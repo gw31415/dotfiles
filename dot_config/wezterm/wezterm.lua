7ad1b267e788a06701216bcde5515a8a4552d4ef
gw31415 <git@amas.dev>
2024-03-30 16:02:52 +0900

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,8 +5,10 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	font = wezterm.font("HackGen Console NF"),
 	font_size = 14,
-	initial_cols = 150,
-	initial_rows = 50,
+	initial_cols = 180,
+	initial_rows = 52,
 	front_end = "WebGpu",
 	macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 20,
 }
