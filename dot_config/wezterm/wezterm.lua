f9452d6ab11adf3851dcfb9a28f545ea174af90a
gw31415 <git@amas.dev>
2024-04-29 14:40:50 +0900

    Add toggle blur functionality and key bindings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,17 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
+
+local function toggle_blur(window)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.macos_window_background_blur then
+		overrides.macos_window_background_blur = 0
+	else
+		overrides.macos_window_background_blur = nil
+	end
+	window:set_config_overrides(overrides)
+end
+
+wezterm.on("toggle-blur", toggle_blur)
+
 return {
 	color_scheme = "Tomorrow Night",
 	default_prog = { "/opt/homebrew/bin/fish", "-l" },
@@ -11,4 +24,106 @@ return {
 	macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
 	window_background_opacity = 0.8,
 	macos_window_background_blur = 20,
+	keys = {
+		{
+			key = "]",
+			mods = "SUPER",
+			action = wezterm.action {
+				ActivateTabRelative = 1,
+			},
+		},
+		{
+			key = "[",
+			mods = "SUPER",
+			action = wezterm.action {
+				ActivateTabRelative = -1,
+			},
+		},
+		{
+			key = "]",
+			mods = "SUPER|SHIFT",
+			action = wezterm.action {
+				MoveTabRelative = 1,
+			},
+		},
+		{
+			key = "[",
+			mods = "SUPER|SHIFT",
+			action = wezterm.action {
+				MoveTabRelative = -1,
+			},
+		},
+		{
+			key = "s",
+			mods = "SUPER",
+			action = wezterm.action {
+				SplitHorizontal = {
+					domain = "CurrentPaneDomain",
+				},
+			},
+		},
+		{
+			key = "s",
+			mods = "SUPER|SHIFT",
+			action = wezterm.action {
+				SplitVertical = {
+					domain = "CurrentPaneDomain",
+				},
+			},
+		},
+		{
+			key = "s",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.PaneSelect {
+				alphabet = "aoeuhtns"
+			},
+		},
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.AdjustPaneSize {
+				'Left', 3,
+			},
+		},
+		{
+			key = "j",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.AdjustPaneSize {
+				'Down', 3,
+			},
+		},
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.AdjustPaneSize {
+				'Up', 3,
+			},
+		},
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.AdjustPaneSize {
+				'Right', 3,
+			},
+		},
+		{
+			key = "w",
+			mods = "SUPER",
+			action = wezterm.action {
+				CloseCurrentPane = {
+					confirm = true,
+				},
+			},
+		},
+		{
+			key = "b",
+			mods = "SUPER",
+			action = wezterm.action { EmitEvent = "toggle-blur" },
+		},
+		{
+			key = " ",
+			mods = "ALT",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
 }
