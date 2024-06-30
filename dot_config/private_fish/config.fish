cd3eaa066353f44949aca4db77e2b3f9d1b03fac
gw31415 <gw31415@amas.dev>
2024-06-30 13:37:48 +0900

    Add JAVA_HOME environment variable for Android Studio JDK

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -21,6 +21,10 @@ if type -q mise
 	end
 end
 
+if test -d /Applications/Android\ Studio.app/Contents/jbr/Contents/Home
+	export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jbr/Contents/Home
+end
+
 source "$HOME/.cargo/env.fish"
 
 abbr neov neovide
