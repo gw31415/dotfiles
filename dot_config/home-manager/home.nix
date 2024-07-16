5655c4381eecfe9410f9083714b8a61564e5c9e9
gw31415 <gw31415@amas.dev>
2024-07-16 11:38:14 +0900

    Update LaTeX configuration in home.nix file

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -51,16 +51,19 @@ let name = "ama"; in
   ];
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".latexmkrc".text = ''
+      #!/usr/bin/env perl
+      $latex            = 'uplatex -halt-on-error -synctex=1 -interaction=nonstopmode';
+      $latex_silent     = 'uplatex -halt-on-error -synctex=1 -interaction=nonstopmode';
+      $bibtex           = 'upbibtex %O %B';
+      $biber            = 'biber --bblencoding=utf8 -u -U --output_safechars';
+      $dvipdf           = 'dvipdfmx %O -o %D %S';
+      $makeindex        = 'mendex %O -o %D %S';
+      $max_repeat       = 5;
+      $pdf_mode         = 3;
+      @generated_exts   = (@generated_exts, 'dvi', 'synctex.gz', 'bbl');
+    '';
   };
 
   # Home Manager can also manage your environment variables through
