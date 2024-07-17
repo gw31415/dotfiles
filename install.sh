f819fc88a125415f1f1a302e51b26912f687ce31
gw31415 <gw31415@amas.dev>
2024-07-18 03:07:53 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
+set -euC
 # Nix is required to be installed
 if [ -z "$(command -v nix)" ]; then
   echo "Nix is not installed. Please install Nix first."
   exit 1
+fi
+
+# Get Nix configuration and check experimental-features
+FEATURES=$(nix show-config | grep experimental-features | awk -F '=' '{print $2}')
+if [[ -z "$FEATURES" ]]; then
+  echo "experimental-features is not set."
+  exit 1
+else
+  if [[ "$FEATURES" != *"nix-command"* || "$FEATURES" != *"flakes"* ]]; then
+    echo "nix-command and/or flakes are not set."
+    exit 1
+  fi
 fi
 
 # Obtaining various variables
