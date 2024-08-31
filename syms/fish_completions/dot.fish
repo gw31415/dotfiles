05b87bdb9420ce759ec1f11dd5512c3bda6a7554
gw31415 <gw31415@amas.dev>
2024-09-01 03:00:49 +0900

    feat: full-featured dot-sh completion

--- syms/fish_completions/dot.fish
+++ syms/fish_completions/dot.fish
@@ -1,21 +1,20 @@
 complete -c dot -f
 
-function __dot_is_root_cmd
+function __fish_dot_is_root_cmd
     for cmd in gc sh # <--- All subcommands
         if __fish_seen_subcommand_from $cmd
             return 1
         end
     end
-    return 0
 end
 
-complete -c dot -n "__dot_is_root_cmd" -l home   -s h -d "Switch env of home-manager"
-complete -c dot -n "__dot_is_root_cmd" -l darwin -s d -d "Switch env of nix-darwin"
-complete -c dot -n "__dot_is_root_cmd" -l all    -s a -d "Switch all envs"
-complete -c dot -n "__dot_is_root_cmd" -l update -s u -d "fetch & update the flake.lock"
+complete -c dot -n "__fish_dot_is_root_cmd" -l home   -s h -d "Switch env of home-manager"
+complete -c dot -n "__fish_dot_is_root_cmd" -l darwin -s d -d "Switch env of nix-darwin"
+complete -c dot -n "__fish_dot_is_root_cmd" -l all    -s a -d "Switch all envs"
+complete -c dot -n "__fish_dot_is_root_cmd" -l update -s u -d "fetch & update the flake.lock"
 
 
-function __fish_complete_dot_sh_subcommand
+function __fish_complete_dot_subcommand
     set subcmd (string sub -s 4 (string match -r 'sh \\S.+ ' (commandline -c))) >/dev/null 2>&1
 	if test $status -ne 0
 		set subcmd ""
@@ -23,8 +22,17 @@ function __fish_complete_dot_sh_subcommand
     complete -C $subcmd
 end
 
+function __fish_complete_dot_options
+    set subcmd (string sub -s 4 (string match -r 'sh \\S.+ ' (commandline -c))) >/dev/null 2>&1
+	if test $status -ne 0
+		set subcmd ""
+	end
+    complete -C "$subcmd"-
+end
+
 complete -c dot -n __fish_use_subcommand -a sh -d "Enter \$SHELL in the home-manager dir"
-complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_sh_subcommand)"
+complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_subcommand)"
+complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_options)"
 
 complete -c dot -n __fish_use_subcommand -a gc -d "Nix garbage-collection"
 complete -c dot -n '__fish_seen_subcommand_from gc' -l aggressive -d 'Aggressive garbage collection'
