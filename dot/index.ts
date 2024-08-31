979129d6dc310ba253d985e94f495ad5407d779b
gw31415 <gw31415@amas.dev>
2024-08-31 14:31:43 +0900

    update error handling

--- dot/index.ts
+++ dot/index.ts
@@ -50,19 +50,20 @@ try {
 	);
 	const homeManagerPath = configHome.join("home-manager");
 	const envnix = homeManagerPath.join("env.nix");
+	const installed = homeManagerPath.join("flake.nix").existsSync();
 
 	////////////////////////////////////////
-	// SHELL with Changed Directory
+	// Subcommands & Default
 	////////////////////////////////////////
 	if (eq(argv.positionals, ["sh"])) {
+		////////////////////////////////////////
+		// SHELL with Changed Directory
+		////////////////////////////////////////
 		await $`${Deno.env.get("SHELL") ?? "/bin/bash"}`.cwd(homeManagerPath);
-		Deno.exit(0);
-	}
-
-	////////////////////////////////////////
-	// Clean up
-	////////////////////////////////////////
-	if (eq(argv.positionals, ["gc"])) {
+	} else if (eq(argv.positionals, ["gc"])) {
+		////////////////////////////////////////
+		// Clean up
+		////////////////////////////////////////
 		consola.info("Cleaning up...");
 		if (argv.values.aggressive) {
 			await $`nix-collect-garbage -d`;
@@ -71,124 +72,123 @@ try {
 			await $`nix store gc -v`;
 			consola.success("Cleaned up.");
 		}
-		Deno.exit(0);
-	}
+	} else if (eq(argv.positionals, [])) {
+		////////////////////////////////////////
+		// Installation & Switching
+		////////////////////////////////////////
+		if (!installed) {
+			consola.box("dot by @gw31415");
+			consola.info("GitHub: https://github.com/gw31415/dotfiles");
 
-	////////////////////////////////////////
-	// Installation & Switching
-	////////////////////////////////////////
-	const installed = homeManagerPath.join("flake.nix").existsSync();
-	if (!installed) {
-		consola.box("dot by @gw31415");
-		consola.info("GitHub: https://github.com/gw31415/dotfiles");
+			consola.info("Starting the setup process...");
+			consola.info("Checking the environment...");
 
-		consola.info("Starting the setup process...");
-		consola.info("Checking the environment...");
-
-		// Check if Nix is installed
-		if (!$.commandExistsSync("nix")) {
-			throw "Nix is not installed. Please install Nix first.";
-		}
-		consola.success("Nix is installed.");
-
-		// Get Nix configuration and check experimental-features
-		const experimental_features =
-			await $`nix show-config | grep experimental-features`.text();
-
-		if (!experimental_features.includes("=")) {
-			throw "experimental-features is not set.";
-		}
-
-		const features = experimental_features
-			.slice(experimental_features.indexOf("=") + 1)
-			.split(/\s+/g)
-			.filter((x) => x.length > 0);
-
-		if (!features.includes("nix-command") || !features.includes("flakes")) {
-			throw "nix-command and/or flakes are not set.";
-		}
-		consola.success("nix-command and flakes are set.");
-
-		// Obtaining various variables
-		consola.info(`Installing path: ${homeManagerPath}`);
-
-		// Cloning the dotfiles
-		if (homeManagerPath.existsSync()) {
-			throw `The target path ${homeManagerPath} already exists. Please remove it first.`;
-		}
-
-		consola.info("Cloning the dotfiles...");
-		configHome.mkdirSync();
-		await $`nix run nixpkgs#git -- clone https://github.com/gw31415/dotfiles ${homeManagerPath}`;
-		consola.success(`Downloaded dotfiles to ${homeManagerPath}.`);
-
-		console.log("");
-		console.log(`> ${envnix}`);
-		console.log(
-			highlight(
-				await Deno.readTextFile(homeManagerPath.join("env.nix").toString()),
-				{
-					language: "nix",
-					ignoreIllegals: false,
-				},
-			),
-		);
-		console.log("");
-		consola.info(
-			`If the information in ${envnix} does not match, the build will fail.`,
-			`You can edit ${envnix} before running the next command.`,
-		);
-		if (
-			!(await consola.prompt("Continue installation?", {
-				type: "confirm",
-			}))
-		) {
-			const remove =
-				((await consola.prompt(`Do you want to clean ${homeManagerPath}?`, {
-					type: "select",
-					options: [
-						{
-							label: `Leave ${envnix}`,
-							value: "leave",
-							hint: `You can edit ${envnix} and continue the installation.`,
-						},
-						{
-							label: `Remove ${envnix}`,
-							value: "remove",
-							hint: "You can re-run the script to download the dotfiles again.",
-						},
-					],
-					initial: "leave",
-				})) as unknown) === "remove";
-			if (remove) {
-				homeManagerPath.removeSync({ recursive: true });
-				consola.info("Removed the downloaded dotfiles.");
-			} else {
-				consola.info(
-					`Please edit ${envnix} and re-run the script to continue the installation.`,
-				);
+			// Check if Nix is installed
+			if (!$.commandExistsSync("nix")) {
+				throw "Nix is not installed. Please install Nix first.";
 			}
-			Deno.exit(0);
-		}
-	}
-	if (argv.values.update) {
-		consola.info("Updating flake.lock...");
-		await $`nix flake update ${homeManagerPath}`;
-		consola.success(`Updated ${homeManagerPath.join("flake.lock")}.`);
-	}
+			consola.success("Nix is installed.");
 
-	// Installing/Upgrading home-manager and initial sync
-	consola.info("Switching home-manager...");
-	const res = await $`nix run nixpkgs#home-manager -- switch`;
-	if (res.code === 0) {
-		consola.success("Success.");
+			// Get Nix configuration and check experimental-features
+			const experimental_features =
+				await $`nix show-config | grep experimental-features`.text();
+
+			if (!experimental_features.includes("=")) {
+				throw "experimental-features is not set.";
+			}
+
+			const features = experimental_features
+				.slice(experimental_features.indexOf("=") + 1)
+				.split(/\s+/g)
+				.filter((x) => x.length > 0);
+
+			if (!features.includes("nix-command") || !features.includes("flakes")) {
+				throw "nix-command and/or flakes are not set.";
+			}
+			consola.success("nix-command and flakes are set.");
+
+			// Obtaining various variables
+			consola.info(`Installing path: ${homeManagerPath}`);
+
+			// Cloning the dotfiles
+			if (homeManagerPath.existsSync()) {
+				throw `The target path ${homeManagerPath} already exists. Please remove it first.`;
+			}
+
+			consola.info("Cloning the dotfiles...");
+			configHome.mkdirSync();
+			await $`nix run nixpkgs#git -- clone https://github.com/gw31415/dotfiles ${homeManagerPath}`;
+			consola.success(`Downloaded dotfiles to ${homeManagerPath}.`);
+
+			console.log("");
+			console.log(`> ${envnix}`);
+			console.log(
+				highlight(
+					await Deno.readTextFile(homeManagerPath.join("env.nix").toString()),
+					{
+						language: "nix",
+						ignoreIllegals: false,
+					},
+				),
+			);
+			console.log("");
+			consola.info(
+				`If the information in ${envnix} does not match, the build will fail.`,
+				`You can edit ${envnix} before running the next command.`,
+			);
+			if (
+				!(await consola.prompt("Continue installation?", {
+					type: "confirm",
+				}))
+			) {
+				const remove =
+					((await consola.prompt(`Do you want to clean ${homeManagerPath}?`, {
+						type: "select",
+						options: [
+							{
+								label: `Leave ${envnix}`,
+								value: "leave",
+								hint: `You can edit ${envnix} and continue the installation.`,
+							},
+							{
+								label: `Remove ${envnix}`,
+								value: "remove",
+								hint: "You can re-run the script to download the dotfiles again.",
+							},
+						],
+						initial: "leave",
+					})) as unknown) === "remove";
+				if (remove) {
+					homeManagerPath.removeSync({ recursive: true });
+					consola.info("Removed the downloaded dotfiles.");
+				} else {
+					consola.info(
+						`Please edit ${envnix} and re-run the script to continue the installation.`,
+					);
+				}
+			}
+		}
+		if (argv.values.update) {
+			consola.info("Updating flake.lock...");
+			await $`nix flake update ${homeManagerPath}`;
+			consola.success(`Updated ${homeManagerPath.join("flake.lock")}.`);
+		}
+
+		// Installing/Upgrading home-manager and initial sync
+		consola.info("Switching home-manager...");
+		const res = await $`nix run nixpkgs#home-manager -- switch`;
+		if (res.code === 0) {
+			consola.success("Success.");
+		} else {
+			throw `Failed to ${installed ? "update" : "install"} home-manager.`;
+		}
+		if (darwin && argv.values.darwin) {
+			consola.info("Switching darwin-rebuild...");
+			await $`nix run github:LnL7/nix-darwin -- switch --flake ${homeManagerPath}`;
+			consola.success("Success.");
+		}
 	} else {
-		throw `Failed to ${installed ? "update" : "install"} home-manager.`;
-	}
-	if (darwin && argv.values.darwin) {
-		consola.info("Switching darwin-rebuild...");
-		await $`nix run github:LnL7/nix-darwin -- switch --flake ${homeManagerPath}`;
-		consola.success("Success.");
+		throw "Unknown subcommand.";
 	}
 } catch (e) {
 	if (typeof e === "string") {
