73973ff46cd2612e3560bfc27b08a2137965c18f
gw31415 <gw31415@amas.dev>
2024-08-31 22:50:15 +0900

    fix: fix and add completion of dot-sh. NOTE: The options are not yet complete.

--- syms/fish_completions/dot.fish
+++ syms/fish_completions/dot.fish
@@ -4,6 +4,14 @@ complete -c dot -n "not __fish_seen_subcommand_from $commands" -l update -s u -d
 complete -c dot -n "not __fish_seen_subcommand_from $commands" -l darwin -d "Switch also with nix-darwin (not only home-manager)"
 
 
+function __fish_complete_dot_subcommand
+    set subcmd (string sub -s 4 (string match -r 'sh \\S.+ ' (commandline -c))) >/dev/null 2>&1
+	if test $status -ne 0
+		set subcmd ""
+	end
+    complete -C $subcmd
+end
+
 complete -c dot -n __fish_use_subcommand -a sh -d "Enter \$SHELL in the home-manager dir"
 complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_subcommand)"
 
