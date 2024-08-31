d120417efb83e40ef28c40068015c7163679e1ac
gw31415 <gw31415@amas.dev>
2024-09-01 03:05:48 +0900

    refactor: update dot.fish

--- syms/fish_completions/dot.fish
+++ syms/fish_completions/dot.fish
@@ -13,26 +13,17 @@ complete -c dot -n "__fish_dot_is_root_cmd" -l darwin -s d -d "Switch env of nix
 complete -c dot -n "__fish_dot_is_root_cmd" -l all    -s a -d "Switch all envs"
 complete -c dot -n "__fish_dot_is_root_cmd" -l update -s u -d "fetch & update the flake.lock"
 
-
-function __fish_complete_dot_subcommand
+function __fish_complete_dot-sh_argcmd
     set subcmd (string sub -s 4 (string match -r 'sh \\S.+ ' (commandline -c))) >/dev/null 2>&1
 	if test $status -ne 0
 		set subcmd ""
 	end
     complete -C $subcmd
-end
-
-function __fish_complete_dot_options
-    set subcmd (string sub -s 4 (string match -r 'sh \\S.+ ' (commandline -c))) >/dev/null 2>&1
-	if test $status -ne 0
-		set subcmd ""
-	end
     complete -C "$subcmd"-
 end
 
 complete -c dot -n __fish_use_subcommand -a sh -d "Enter \$SHELL in the home-manager dir"
-complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_subcommand)"
-complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot_options)"
+complete -c dot -n '__fish_seen_subcommand_from sh' -a "(__fish_complete_dot-sh_argcmd)"
 
 complete -c dot -n __fish_use_subcommand -a gc -d "Nix garbage-collection"
 complete -c dot -n '__fish_seen_subcommand_from gc' -l aggressive -d 'Aggressive garbage collection'
