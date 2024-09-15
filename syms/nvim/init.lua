94f3900fd635789a769630108b89136398bedb6e
gw31415 <gw31415@amas.dev>
2024-09-15 20:50:06 +0900

    feat: dein to dpp

---
+++ syms/nvim/init.lua
@@ -0,0 +1,124 @@
+vim.opt.runtimepath:prepend '$HOME/.cache/dpp/repos/github.com/Shougo/dpp.vim'
+local dpp = require 'dpp'
+
+local dpp_base = '~/.cache/dpp/'
+
+vim.opt.runtimepath:append '$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml'
+vim.opt.runtimepath:append '$HOME/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git'
+vim.opt.runtimepath:append '$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy'
+vim.opt.runtimepath:append '$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer'
+
+if dpp.load_state(dpp_base) then
+    vim.opt.runtimepath:prepend '$HOME/.cache/dpp/repos/github.com/vim-denops/denops.vim'
+
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'DenopsReady',
+        callback = function()
+            vim.notify 'vim load_state is failed'
+            dpp.make_state(dpp_base, '~/.config/nvim/dpp.ts')
+        end
+    })
+end
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'Dpp:makeStatePost',
+    callback = function()
+        vim.notify 'dpp make_state() is done'
+    end
+})
+
+
+------------------------
+-- Personal settings
+------------------------
+vim.api.nvim_create_autocmd('CursorHold', {
+    once = true,
+    command = 'source $HOME/.config/nvim/lazy.vim'
+})
+
+vim.opt_global.encoding = 'utf-8'
+vim.opt_global.fencs = { 'utf-8', 'iso-2022-jp', 'euc-jp', 'sjis' }
+vim.g.loaded_netrwPlugin = true
+vim.g.loaded_remote_plugins = true
+vim.g.skip_loading_mswin = true
+vim.g.loaded_tutor_mode_plugin = true
+vim.g.loaded_2html_plugin = true
+vim.o.winblend = 20
+vim.o.pumblend = 20
+vim.wo.number = true
+vim.go.guifont = 'HackGen_Console_NF:h14'
+
+vim.cmd [[
+se ut=1
+au CursorHold * ++once se ut=4000
+autocmd BufRead *.typ setfiletype typst
+autocmd BufRead *.tf setfiletype terraform
+colorscheme onedark
+]]
+
+function _G.get_warn_count()
+    local warns = vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.WARN })
+    return #warns
+end
+
+function _G.get_error_count()
+    local errors = vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.ERROR })
+    return #errors
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function()
+        vim.opt_local.formatexpr = 'v:lua.vim.lsp.formatexpr(#{timeout_ms:250})'
+
+        vim.diagnostic.config { signs = false }
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+            vim.lsp.handlers.hover,
+            { border = 'single', title = 'hover' }
+        )
+        vim.api.nvim_create_user_command('Implementation', function()
+            vim.lsp.buf.implementation()
+        end, { force = true })
+        local bufopts = { silent = true, buffer = true }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', function()
+            local winid = require 'ufo'.peekFoldedLinesUnderCursor()
+            if not winid then
+                vim.lsp.buf.hover()
+            end
+        end, bufopts)
+        -- vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, bufopts) -- Now default mapping is `]d`
+        -- vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, bufopts) -- Now default mapping is `[d`
+        vim.keymap.set('n', 'glr', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gln', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', 'z*', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('i', '<C-S>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', 'gqae', function()
+                local view = vim.fn.winsaveview()
+                vim.lsp.buf.format { async = false }
+                if view then vim.fn.winrestview(view) end
+            end,
+            { buffer = true }
+        )
+        -- refresh codelens on TextChanged and InsertLeave as well
+        -- vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave', 'CursorHold', 'LspAttach' }, {
+        -- 	buffer = 0,
+        -- 	callback = vim.lsp.codelens.refresh,
+        -- })
+
+        -- trigger codelens refresh
+        -- vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+        -- vim.api.nvim_create_autocmd('BufWritePre', {
+        -- 	callback = function() vim.lsp.buf.format { async = false } end,
+        -- 	buffer = bufnr,
+        -- })
+    end,
+})
+
+-- Ignore startup treesitter errors
+vim.treesitter.start = (function(wrapped)
+    return function(bufnr, lang)
+        lang = lang or vim.fn.getbufvar(bufnr or '', '&filetype')
+        pcall(wrapped, bufnr, lang)
+    end
+end)(vim.treesitter.start)
---
+++ syms/nvim/lazy.vim
@@ -0,0 +1,50 @@
+se fdc=1
+se vop=folds
+se ts=4
+se sw=4
+se cul
+se cuc
+se sb
+se hid
+se ls=3
+se sms
+se so=3
+se ch=0
+se diffopt+=algorithm:histogram
+se stl=%f%r%m%h%w%=%{&et?'(et)':''}sw=%{&sw}\ E%{v:lua.get_error_count()}W%{v:lua.get_warn_count()}\ %l/%L
+
+ino <c-f> <c-g>U<right>
+ino <c-b> <c-g>U<left>
+ino <c-p> <c-g>U<up>
+ino <c-n> <c-g>U<down>
+ino <c-d> <c-g>U<del>
+ino <expr> <c-a> col('.') == match(getline('.'), '\S') + 1 ?
+	\ repeat('<C-G>U<Left>', col('.') - 1) :
+	\ (col('.') < match(getline('.'), '\S') ?
+	\     repeat('<C-G>U<Right>', match(getline('.'), '\S') + 0) :
+	\     repeat('<C-G>U<Left>', col('.') - 1 - match(getline('.'), '\S')))
+ino <expr> <c-e> repeat('<C-G>U<Right>', col('$') - col('.'))
+
+cno <c-f> <right>
+cno <c-b> <left>
+cno <c-p> <up>
+cno <c-n> <down>
+cno <c-d> <del>
+cno <c-a> <home>
+cno <c-e> <end>
+
+" Visual <, >で連続してインデントを操作
+xno < <gv
+xno > >gv
+
+" 空行での編集開始時に自動でインデント
+nno <expr> i empty(getline('.')) ? '"_cc' : 'i'
+
+if executable('rg')
+	set grepprg=rg\ --vimgrep
+	set grepformat=%f:%l:%c:%m
+endif
+
+if executable('mise')
+	lua vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+endif
