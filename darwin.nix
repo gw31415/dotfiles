75ba7edad1d9c5d21d447ab191be833a3f75ccd0
gw31415 <gw31415@amas.dev>
2025-02-01 16:12:07 +0900

    feat: homebrew config set to darwin.nix

--- darwin.nix
+++ darwin.nix
@@ -41,6 +41,89 @@
   };
 
   ########################################
+  # Homebrew
+  ########################################
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    taps = [
+      "homebrew/bundle"
+      "homebrew/cask-fonts"
+      "homebrew/cask-versions"
+      "homebrew/core"
+      "homebrew/services"
+      "jorgelbg/tap"
+      "monochromegane/tap"
+      "mtgto/macskk"
+    ];
+    brews = [
+      "openssl@3"
+      "gettext"
+      "iconsur"
+      "libgpg-error"
+      "mas"
+      "mise"
+      "p7zip"
+      "pinentry-mac"
+      "pkgconf"
+      "jorgelbg/tap/pinentry-touchid"
+      "monochromegane/tap/afa"
+      "monochromegane/tap/afa-tui"
+    ];
+    casks = [
+      "alt-tab"
+      "android-studio"
+      "anki"
+      "battery"
+      "brave-browser"
+      "chatgpt"
+      "devtoys"
+      "discord"
+      "figma"
+      "gnucash"
+      "inkscape"
+      "keepassxc"
+      "keybase"
+      "macfuse"
+      "macskk"
+      "microsoft-auto-update"
+      "microsoft-teams"
+      "musicbrainz-picard"
+      "obsidian"
+      "orbstack"
+      "piphero"
+      "postman"
+      "slack"
+      "visual-studio-code"
+      "vlc"
+      "wezterm@nightly"
+      "zoom"
+    ];
+    masApps = {
+      "1Blocker" = 1365531024;
+      "Amphetamine" = 937984704;
+      "Compressor" = 424390742;
+      "Final Cut Pro" = 424389933;
+      "Goodnotes" = 1444383602;
+      "Keynote" = 409183694;
+      "LINE" = 539883307;
+      "Logic Pro" = 634148309;
+      "MainStage" = 634159523;
+      "Mona" = 1659154653;
+      "Motion" = 434290957;
+      "Numbers" = 409203825;
+      "OmniFocus" = 1542143627;
+      "Pages" = 409201541;
+      "RunCat" = 1429033973;
+      "Shortcut Notch" = 6447055708;
+      "Xcode" = 497799835;
+      "タイピスト" = 415166115;
+      "宛名印刷" = 1598123076;
+    };
+  };
+
+  ########################################
   # Auto install software updates
   ########################################
   system.activationScripts.extraActivation.text = ''
