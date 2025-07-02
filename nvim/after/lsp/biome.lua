8459b0de8296257b4981568857c9389833756cda
gw31415 <gw31415@amas.dev>
2025-07-02 21:34:34 +0900

    feat: Support node_modules biome command

--- nvim/after/lsp/biome.lua
+++ nvim/after/lsp/biome.lua
@@ -2,4 +2,10 @@
 return {
 	root_markers = { 'biome.json' },
 	workspace_required = true,
+	cmd = function(dispatchers, config)
+		local root_dir = config.root_dir
+		local local_cmd = root_dir and (root_dir .. '/node_modules/.bin/biome') or ''
+		local command = vim.fn.executable(local_cmd) == 1 and local_cmd or 'biome'
+		return vim.lsp.rpc.start({ command, 'lsp-proxy' }, dispatchers)
+	end,
 }
