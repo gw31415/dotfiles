b775bc9c5132356512b1834d91f84def0235f6b6
gw31415 <24710985+gw31415@users.noreply.github.com>
2025-09-12 12:41:09 +0900

    feat: Add Neovide settings to config

--- nvim/lua/lazy.lua
+++ nvim/lua/lazy.lua
@@ -1,5 +1,15 @@
 vim.opt_global.helplang = 'ja,en'
 
+if vim.g.neovide then
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0.00
+	vim.g.neovide_cursor_trail_size = 0
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_scroll_animation_far_lines = 0
+	vim.g.neovide_scroll_animation_length = 0.00
+end
+
 --------------------------------------------------------------------------------
 -- Global Functions used in Statusline
 --------------------------------------------------------------------------------
