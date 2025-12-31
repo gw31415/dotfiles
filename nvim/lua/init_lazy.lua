5c0fcd566c4a23d3cbfd1a8bf091d75084d4a1ba
gw31415 <24710985+gw31415@users.noreply.github.com>
2025-12-31 18:00:05 +0900

    feat: Add QuitPre autocmd for special windows

--- nvim/lua/init_lazy.lua
+++ nvim/lua/init_lazy.lua
@@ -143,6 +143,31 @@ vim.api.nvim_create_user_command('Restart', function()
 	vim.cmd('restart source ' .. vim.fn.fnameescape(cache))
 end, {})
 
+-- 残りのウィンドウが特殊ウィンドウのみである場合、終了する
+-- https://zenn.dev/vim_jp/articles/ff6cd224fab0c7
+vim.api.nvim_create_autocmd('QuitPre', {
+	callback = function()
+		-- 現在のウィンドウ番号を取得
+		local current_win = vim.api.nvim_get_current_win()
+		-- すべてのウィンドウをループして調べる
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			-- カレント以外を調査
+			if win ~= current_win then
+				local buf = vim.api.nvim_win_get_buf(win)
+				-- buftypeが空文字（通常のバッファ）があればループ終了
+				if vim.bo[buf].buftype == '' then
+					return
+				end
+			end
+		end
+		-- ここまで来たらカレント以外がすべて特殊ウィンドウということなので
+		-- カレント以外をすべて閉じる
+		vim.cmd.only({ bang = true })
+		-- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
+	end,
+	desc = 'Close all special buffers and quit Neovim',
+})
+
 -- local ok, extui = pcall(require, 'vim._extui')
 -- if ok then
 -- 	extui.enable({
