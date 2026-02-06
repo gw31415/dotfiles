8fbc1f6d2d330acc37bf9491f624b06e7ea20fca
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-02-06 23:31:52 +0900

    feat: update rsplug.fish

--- syms/fish_completions/rsplug.fish
+++ syms/fish_completions/rsplug.fish
@@ -15,8 +15,3 @@ complete -c rsplug      -l locked -d "Fix the repo version with rev in the lockf
 complete -c rsplug -l lockfile -r -F -d "Specify the lockfile path" -n "not __fish_seen_argument -l lockfile"
 
 complete -c rsplug -s h -l help -d 'Print help'
-
-# --- Arguments ---
-# CONFIG_FILES are glob-patterns; fish already supports globs, so we just accept files.
-# (If your patterns can include ':' as a separator, users will type it explicitly.)
-complete -c rsplug -a "(__fish_complete_path)" -d "Config file glob-pattern(s)"
