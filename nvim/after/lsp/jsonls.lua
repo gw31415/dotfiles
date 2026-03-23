2230587f14e560e0a6b985f2ad1f129d031785f7
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-03-23 15:16:29 +0900

    fix: Update LSP config for JSON formatter

--- nvim/after/lsp/jsonls.lua
+++ nvim/after/lsp/jsonls.lua
@@ -2,7 +2,13 @@
 return {
 	on_attach = function(client, bufnr)
 		local biomeRunning = #(vim.lsp.get_clients { name = 'biome', bufnr = bufnr }) > 0
-		client.server_capabilities.documentFormattingProvider = not biomeRunning
-		client.server_capabilities.documentRangeFormattingProvider = not biomeRunning
+		local hasFormatterConfigFile = vim.fs.find({
+				'biome.json',
+				'.biome.json',
+				'.oxfmtrc.json',
+			},
+			{ path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)), upward = true })[1] ~= nil
+		client.server_capabilities.documentFormattingProvider = not (biomeRunning or hasFormatterConfigFile)
+		client.server_capabilities.documentRangeFormattingProvider = not (biomeRunning or hasFormatterConfigFile)
 	end,
 }
